f28ee62dc6bed41dec10c0cfe065c340746741f5
Kevin Washington <kevw12188@gmail.com>
2015-09-21 11:19:25 -0400

    added functionality to determine if number is a prime, also added functionality to add all prime numbers within range

--- algorithm.js
+++ algorithm.js
@@ -1,4 +1,28 @@
 function isPrime(numberToCheck) {
+    if(numberToCheck == 1) return false;
     if(numberToCheck == 2) return true;
-    return (numberToCheck % 2 != 0);
+    var counter = 2;
+    var limit = Math.sqrt(numberToCheck);
+    Math.round(limit);
+    var result = true;
+    while(counter <= limit) {
+        if(numberToCheck % counter == 0) {
+           result = false;
+        }
+        counter++;
+    }
+    return result;
 }
+
+function summationOfPrimes(rangeStart, rangeStop) {
+    var counter = rangeStart;
+    var sumOfPrimes = 0;
+    while(counter < rangeStop) {
+        if(isPrime(counter) == true) {
+            sumOfPrimes += counter;
+        }
+        counter++;
+    }
+    return sumOfPrimes;
+}
+
--- algorithm.test.js
+++ algorithm.test.js
@@ -1,4 +1,10 @@
 describe("isPrime", function() {
+    it("should return false for number 1", function() {
+        var result = isPrime(1);
+
+        correct = false;
+        expect(result).toEqual(correct);
+    });
     it("should return true if number is a prime", function() {
         var result1 = isPrime(2);
         var result2 = isPrime(3);
@@ -16,9 +22,29 @@ describe("isPrime", function() {
         var result3 = isPrime(8);
         var result4 = isPrime(10);
         var result5 = isPrime(12);
+        var result6 = isPrime(9);
 
-        var resultArray = [result1, result2, result3, result4, result5];
-        var correct = [false, false, false, false, false];
+        var resultArray = [result1, result2, result3, result4, result5, result6];
+        var correct = [false, false, false, false, false, false];
+        expect(resultArray).toEqual(correct);
+    });
+    it("should also include numbers greater than 50", function() {
+        var result1 = isPrime(53);
+        var result2 = isPrime(59);
+        var result3 = isPrime(61);
+        var result4 = isPrime(67);
+        var result5 = isPrime(71);
+
+        var resultArray = [result1,result2, result3, result4, result5];
+        var correct = [true, true, true, true, true];
         expect(resultArray).toEqual(correct);
     })
+});
+
+describe("summationOfPrimes", function() {
+    it("should add all primes within specified range and return the sum", function() {
+        var result = summationOfPrimes(1,10);
+        var correct = 17;
+        expect(result).toBe(correct);
+    });
 });
